83065092ea106a51ae7792ace6f62e0c9ff723f8
Nikita <Nikita.kuzin.88@mail.ru>
2023-07-11 22:46:56 +0500

    решили задачу на уроке

--- Siminar1/task1/Program.cs
+++ Siminar1/task1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.Clear();
+Console.Write("Введите номер для недели: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number == 1){
+    Console.WriteLine ($"{number} это понедельник");
+}
+if (number == 2){
+    Console.WriteLine ($"{number} это вторник");
+} 
+if (number == 3){
+    Console.WriteLine ($"{number} это среда");
+} 
+if (number == 4){
+    Console.WriteLine ($"{number} это четверг");
+} 
+if (number == 5){
+    Console.WriteLine ($"{number} это пятница");
+} 
+if (number == 6){
+    Console.WriteLine ($"{number} это суббота");
+} 
+if (number == 7){
+    Console.WriteLine ($"{number} это воскресенье");
+}  
