005319616e72d181e8849489dabad497ba59d40a
Nikita <Nikita.kuzin.88@mail.ru>
2023-07-11 22:17:51 +0500

    add Task0

---
+++ Siminar1/task0/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу которая на вход принимает число и выдает его квадрат
+
+
+// Например 
+// 4 -> 16
+// 3 -> 9
+// 7 -> 14
+
+Console.Clear();
+Console.Write ("Введите число: ");
+int num1 = int.Parse(Console.ReadLine());
+int result = num1 * num1;
+Console.WriteLine($"Квадрат числа {num1} равен {result}" );
